91266c7c2d41304d6dacc81a3ad504baf832e784
Johnny Matos <128853698+DS-Johnny@users.noreply.github.com>
2025-03-18 10:25:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 # ------------------------ Sidebar ------------------------------------------
-st.image("senai_logo.jpg")
+st.sidebar.image("senai_logo.jpg")
+st.sidebar.title('Fábrica de Programadores')
 
 
 
@@ -13,13 +14,13 @@ st.image("senai_logo.jpg")
 col1, col2 = st.columns(2)
 
 with col1:
-	st.image('johnny.jpeg')
+	st.image('johnny.jpeg', width=200)
 
 with col2:
 	st.title('Johnny')
-	st.markdown('Oi, pessoal! Eu sou o professor Johnny e serei o professor de Python de vocês.')
+	st.markdown('## Oi, pessoal! Eu sou o professor Johnny e serei o professor de Python de vocês.')
 
 st.markdown('---')
 
-hist, jornada, sobre = st.columns(['Minha história com a programação', 'A jornada de vocês', 'Curiosidades sobre mim'])
+hist, jornada, sobre = st.tabs(['Minha história com a programação', 'A jornada de vocês', 'Curiosidades sobre mim'])
 
